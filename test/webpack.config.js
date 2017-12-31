62870f33ea8843525445555f371206a7f898a4e5
Joe Wood <joewood@live.com>
2017-12-31 09:42:48 -0500

    Formatting

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,25 +1,18 @@
 const path = require("path");
-var webpack = require('webpack');
-// var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require("webpack");
 
 module.exports = {
-  entry: [
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server'),
-    './test/test.tsx'
-  ],
-  output: {
-    pathinfo: true,
-    filename: './test/lib/test.js',
-    publicPath: '/'
-  },
-  devtool: 'cheap-module-source-map',
-  resolve: {
-    extensions: [ '.webpack.js', '.web.js', '.ts', '.js', '.tsx']
-  },
-  module: {
-    loaders: [
-      { test: /\.ts.?$/, loader: 'ts-loader' }
-    ]
-  },
-}
+    entry: ["./test/test.tsx"],
+    output: {
+        pathinfo: true,
+        filename: "./test/lib/test.js",
+        publicPath: "/"
+    },
+    devtool: "cheap-module-source-map",
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".js", ".tsx"]
+    },
+    module: {
+        loaders: [{ test: /\.ts.?$/, loader: "ts-loader" }]
+    }
+};
